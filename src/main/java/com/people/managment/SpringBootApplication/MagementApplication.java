16dc462310459f7b675a418e37ca1f991872c73c
Túlio Mendes <167912036+TulioMendesDS@users.noreply.github.com>
2024-10-02 00:25:21 -0300

    tests

---
+++ src/main/java/com/people/managment/SpringBootApplication/MagementApplication.java
@@ -0,0 +1,13 @@
+package com.people.management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagementApplication.class, args);
+	}
+
+}
